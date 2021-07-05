f783c293fe9c0f7584874c1c75c7f45ec2e5a62e
Pierre Mulliez <37341575+Pierre-mulliez1@users.noreply.github.com>
2021-07-05 11:43:27 +0200

    EDA created

---
+++ src/cov/__init__.py
@@ -0,0 +1,12 @@
+"""
+Project to predict Covid19 incidence 
+"""
+
+__version__ = "0.1.0"
+
+# +
+if __name__ == "__main__":
+    print("in development")
+    
+def function1(stri):
+    print("this is a test {}".format(str(stri)))
